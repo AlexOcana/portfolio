119c978d495d29209a2abe20e4ff157e3f2fc963
AlexOcana <a.ocana.monsalve@hotmail.com>
2023-10-17 18:46:42 +0200

    final

--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -1,7 +1,28 @@
+import "./ContactPage.css"
+
 const ContactPage = () => {
 
     return (
-        <h1>Contact Page</h1>
+        <>
+            <div className="AllContect">
+                <h1>Contact me if you want me to be on your team !!!" </h1>
+
+                <h6>Please click on the following icons to go to my diferents sites : </h6>
+            </div>
+            <div class="links">
+                <div class="link">
+                    <a className='HoverLinks' href="https://github.com/AlexOcana" target="_blank">
+                        <img src="public/github.png" alt="GitHub"></img>
+                        <h6>GitHub Profile</h6>
+                    </a>
+                    <a className='HoverLinks' href="Alejandro Ocaña.pdf" download><img src="public/pdf.png" alt="Imagen 1" style={{ width: '70px', }} ></img>
+                        <h6>Download my CV</h6></a></div>
+            </div >
+            <div className="Section2">
+                <a href="mailto:ocanamonsalve.alejandro@gmail.com">Send me an email clicking here</a>
+            </div>
+        </>
     )
 }
+
 export default ContactPage
--- src/components/pages/ProyectsDetailsPage.jsx
+++ src/components/pages/ProyectsDetailsPage.jsx
@@ -1,8 +1,54 @@
+import './ProyectsDetailsPage.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 const ProyectsDetailsPage = () => {
 
     return (
         <>
-            <h1>Proyect Details Page</h1>
+            <h2>See the best proyects i have done in my carrer</h2>
+            <div className='AllCards'>
+                <Card className='CardStyle'>
+                    <Card.Img variant="top" src="./public/proyectovideojuego.png" />
+                    <Card.Body className='mt-2 text-center'>
+                        <Card.Title>DOM VideoGame Proyect</Card.Title>
+                        <Card.Text>
+                            DOM-Powered Side-Scrolling Action Game!" Experience the exhilarating world
+                            of side-scrolling action, reminiscent of Metal Slug, all brought to life through HTML,
+                            CSS, and JavaScript. Battle your way through hordes of enemies and explosive encounters
+                            while uncovering the secrets of web development. Get ready for a nostalgia-filled adventure in DOMSlug!
+                        </Card.Text>
+                        <Button variant="warning "><a className='text-decoration-none text-black' href="https://alexocana.github.io/PROYECTO-VIDEOJUEGO/" target='_blanck'>Enter to see</a></Button>
+                    </Card.Body>
+                </Card>
+
+                <Card className='CardStyle2'>
+                    <Card.Img variant="top" src="./public/videogames.jpeg" />
+                    <Card.Body className='mt-2 text-center'>
+                        <Card.Title>HBS Proyect VideoGames API</Card.Title>
+                        <Card.Text>
+                            Web application for searching video games using Node.js,
+                            Axios, and Express. The application allows users to search
+                            for video games and retrieve detailed information about
+                            their favorite titles, making it a valuable resource for
+                            gamers.
+                        </Card.Text>
+                        <Button variant="warning "><a className='text-decoration-none text-black' href="https://github.com/AlexOcana" target='_blanck'>Enter to see</a></Button>
+                    </Card.Body>
+                </Card>
+                <Card className='CardStyle3'>
+                    <Card.Img variant="top" src="./public/gymapp.png" />
+                    <Card.Body className='mt-2 text-center'>
+                        <Card.Title>REACT App for GYM</Card.Title>
+                        <Card.Text>
+                            Application for creating and managing gym routines built
+                            using React, Node.js, MongoDB, Mongoose, and Express
+
+                        </Card.Text>
+                        <Button variant="warning "><a className='text-decoration-none text-black' href="https://gym4life.netlify.app/" target='_blanck'>Enter to see</a></Button>
+                    </Card.Body>
+                </Card>
+            </div>
+
         </>
     )
 }
